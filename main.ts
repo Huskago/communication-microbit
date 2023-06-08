5b1da11ee6fc7193cb9733e25ece8500373c307f
Huskago <tiagoflfl0202@gmail.com>
2023-06-08 13:07:49 +0200

    add distance with onde

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-music.playMelody("- - - - - - - - ", 120)
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    time = time + radio.receivedPacket(RadioPacketProperty.Time)
+    distance = time * 299792458 / 2
+    basic.showNumber(distance)
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("starlink")
+})
+radio.onReceivedString(function (receivedString) {
+    time = radio.receivedPacket(RadioPacketProperty.Time)
+    radio.sendNumber(time)
+})
+let distance = 0
+let time = 0
+radio.setGroup(195)
